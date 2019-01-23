03da06a4ceaf9c5e0ad027aeb9e7bb0e0b3b2636
Alps709 <alps709@gmail.com>
2019-01-24 10:32:36 +1300

    Added persistence, lacunarity, seeds and multiple octaves to the noise texture generator.

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MapDisplay : MonoBehaviour {
 
-	public Renderer textureRenderer;
+	public Renderer textureRender;
 
 	public void DrawNoiseMap(float [,] noiseMap)
 	{
@@ -19,13 +19,13 @@ public class MapDisplay : MonoBehaviour {
 		{
 			for (int x = 0; x < width; x++)
 			{
-				colourMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x,y]);
+				colourMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
 			}
 		}
 		texture.SetPixels(colourMap);
 		texture.Apply();
 
-		textureRenderer.sharedMaterial.mainTexture = texture;
-		textureRenderer.transform.localScale = new Vector3 (width, 1, height);
+		textureRender.sharedMaterial.mainTexture = texture;
+		textureRender.transform.localScale = new Vector3 (width, 1, height);
 	}
 }
